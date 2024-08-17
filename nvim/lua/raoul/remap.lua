01ddda0a5bda7d01e13793845a0d8e6c9723e02a
rverspaij <103568731+rverspaij@users.noreply.github.com>
2024-08-17 13:08:55 +0200

    Add files via upload

---
+++ nvim/lua/raoul/remap.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Search for word under curser in all files
+vim.keymap.set('n', '<leader>f', ':lua vim.cmd("execute \'vimgrep /" .. vim.fn.expand("<cword>") .. "/ **/*\'")<CR>:copen<CR>', { noremap = true, silent = true })
+
+-- Switch between terminals
+vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
+
+-- Open cheatsheet file for neovim
+vim.keymap.set('n', '<leader>ch', ':e ~/Documents/nvim_cheatsheet.txt<CR>')
